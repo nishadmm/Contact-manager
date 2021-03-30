6a8c4d1347351ebf5effff3216c4a44f2585765d
nishad <nishadmm5461@gmail.com>
2021-03-30 14:49:40 +0530

    initila commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { HashRouter as Router, Route, Switch } from 'react-router-dom'
 import Header from './components/Header'
 import AddForm from './components/AddForm'
 import EditContact from './components/EditContact'
